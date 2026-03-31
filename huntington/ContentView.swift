9d49a20aa320d92cbf6aec5de69712e9b6cd9453
Kieran Klukas <me@dunkirk.sh>
2026-03-31 06:18:20 -0400

    Initial Commit

---
+++ huntington/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  huntington
+//
+//  Created by Kieran Klukas on 3/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ huntington/huntingtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  huntingtonApp.swift
+//  huntington
+//
+//  Created by Kieran Klukas on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct huntingtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
